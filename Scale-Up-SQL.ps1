e8e4160d8165b5f8b74e58a150d51e72bf36573c
eolver <46315332+eolver@users.noreply.github.com>
2019-10-17 20:59:02 +1100

    Create Scale-Up-SQL.ps1

---
+++ Scale-Up-SQL.ps1
@@ -0,0 +1,36 @@
+$Conn = Get-AutomationConnection -Name 'AzureRunAsConnection'
+Connect-AzureRmAccount -ServicePrincipal -Tenant $Conn.TenantID `
+-ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
+
+
+#Resource group and Servername to be defined
+
+$rg="<resourcegroupname>"
+$svr="<SQL Server Name>"
+
+
+#Change the Tier for the SQL Server
+
+$Edition="Standard"
+# Desired performance level {Basic, S0, S1, S2, P1, P2, P3}
+$PerfLevel="S1"
+
+#For each Database if not the 'master' scale to the desired performance level
+
+$dbs=Get-AzureRmSqlDatabase -servername $svr -resourcegroup $rg
+
+foreach($db in $dbs)
+{
+if($db.DatabaseName -ne "master")
+{ 
+Set-AzurermSqlDatabase -ResourceGroupName $db.ResourceGroupName `
+-ServerName $db.ServerName `
+-DatabaseName $db.DatabaseName `
+-Edition $Edition `
+-RequestedServiceObjectiveName $PerfLevel
+}
+} 
+
+
+
+    Write-Output "Scale up  DB to S1" 
