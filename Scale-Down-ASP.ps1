faa467e4f6385cc6c7755021af4e4c285eea0a3b
eolver <46315332+eolver@users.noreply.github.com>
2019-10-17 21:04:37 +1100

    Create Scale-Down-ASP.ps1

---
+++ Scale-Down-ASP.ps1
@@ -0,0 +1,22 @@
+$Conn = Get-AutomationConnection -Name 'AzureRunAsConnection'
+Connect-AzureRmAccount -ServicePrincipal -Tenant $Conn.TenantID `
+-ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
+
+
+#define the resource parameters
+$rg="<resourcegroupname>"
+$Tier="Free"
+$worker="Small"
+
+$apps=Get-AzureRmAppServicePlan -ResourceGroupName $rg 
+
+$apps
+
+#for every App Service Plan in the ResourceGroup change to the defined Tier and workersize.
+
+foreach($app in $apps)
+{
+$appname=$app.name
+
+Set-AzureRmAppServicePlan -Name $appname -ResourceGroupName $rg -Tier $Tier 
+}
